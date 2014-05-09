6e866764813164137d512354f424c8497f05283d
John Carr <john.carr@unrouted.co.uk>
2014-05-09 10:15:00 +0100

    Fix py3

--- fuselage/providers/filesystem/__init__.py
+++ fuselage/providers/filesystem/__init__.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import files
-import link
-import directory
+from . import files
+from . import link
+from . import directory
 
 __all__ = [
     "files",
