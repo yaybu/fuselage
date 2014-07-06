1af5e85325db82ccc8ae984d8dc48a8cf3ed5f16
John Carr <john.carr@unrouted.co.uk>
2014-07-06 11:25:43 +0100

    Reformat setup.py and advertise py3/pypyp support in classifiers

--- setup.py
+++ setup.py
@@ -3,31 +3,39 @@ import os
 
 version = '0.0.dev0'
 
-setup(name='fuselage',
-      version=version,
-      url="http://yaybu.com/",
-      description="Server deployment and configuration management in Python",
-      long_description = open("README.rst").read() + "\n" + \
-                         open("CHANGES").read(),
-      author="Isotoma Limited",
-      author_email="support@isotoma.com",
-      license="Apache Software License",
-      classifiers = [
-          "Intended Audience :: System Administrators",
-          "Operating System :: POSIX",
-          "License :: OSI Approved :: Apache Software License",
-      ],
-      packages=find_packages(exclude=['ez_setup']),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'six',
-      ],
-      extras_require = {
-          'test': ['unittest2', 'mock', 'fakechroot'],
-          },
-      entry_points = """
-      [console_scripts]
-      fuselage = fuselage.main:main
-      """
-      )
+setup(
+    name='fuselage',
+    version=version,
+    url="http://yaybu.com/",
+    description="Server deployment and configuration management in Python",
+    long_description = open("README.rst").read(),
+    author="Isotoma Limited",
+    author_email="support@isotoma.com",
+    license="Apache Software License",
+    classifiers = [
+        "Intended Audience :: System Administrators",
+        "Operating System :: POSIX",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    packages=find_packages(exclude=['ez_setup']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'six',
+    ],
+    extras_require = {
+        'test': ['unittest2', 'mock', 'fakechroot'],
+        },
+    entry_points = """
+    [console_scripts]
+    fuselage = fuselage.main:main
+    """
+)
