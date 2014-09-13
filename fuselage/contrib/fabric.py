02da509489a07aa61ad3ce7ed42df3c76f076fc5
John Carr <john.carr@unrouted.co.uk>
2014-09-13 13:41:54 +0100

    Prototype Docker container building

--- fuselage/contrib/fabric.py
+++ fuselage/contrib/fabric.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from __future__ import absolute_import
+from __future__ import absolute_import, print_function
 import six
 
 try:
@@ -26,14 +26,11 @@ except SyntaxError:
 from fuselage import bundle, builder, error
 
 
-class DeploymentTask(tasks.WrappedCallableTask):
+class FuselageMixin(object):
 
-    def run(self, *args, **kwargs):
-        unsupported = set(kwargs.keys()).difference(set(["simulate"]))
-        if unsupported:
-            utils.error("The following parameters were not understood: %s" % ", ".join(unsupported))
-            return
+    arguments = []
 
+    def get_bundle_stream(self, *args, **kwargs):
         try:
             bun = bundle.ResourceBundle()
             bun.extend(self.wrapped(bun, *args, **kwargs))
@@ -49,7 +46,43 @@ class DeploymentTask(tasks.WrappedCallableTask):
         bu.embed_resource_bundle(bun)
         bu.close()
 
-        uploaded = put(buffer, '~/payload.pex', mode=755)
+        buffer.seek(0)
+
+        return buffer
+
+    def apply_bundle(self, buffer, *args, **kwargs):
+        raise NotImplementedError(self.apply_bundle)
+
+    def run(self, *args, **kwargs):
+        unsupported = set(kwargs.keys()).difference(set(self.arguments))
+        if unsupported:
+            utils.error("The following parameters were not understood: %s" % ", ".join(unsupported))
+            return
+
+        return self.apply_bundle(self.get_bundle_stream(*args, **kwargs), *args, **kwargs)
+
+    def __call__(self, *args, **kwargs):
+        return self.run(*args, **kwargs)
+
+    @classmethod
+    def as_decorator(cls):
+        def _(*args, **kwargs):
+            invoked = bool(not args or kwargs)
+            if not invoked:
+                func, args = args[0], ()
+            task_class = kwargs.pop("task_class", cls)
+            def wrapper(func):
+                return task_class(func, *args, **kwargs)
+            return wrapper if invoked else wrapper(func)
+        return _
+
+
+class DeploymentTask(FuselageMixin, tasks.WrappedCallableTask):
+
+    arguments = ['simulate']
+
+    def apply_bundle(self, bundle, *args, **kwargs):
+        uploaded = put(bundle, '~/payload.pex', mode=755)
         if uploaded.failed:
             utils.error("Could not upload fuselange bundle to target. Aborting.")
             return
@@ -70,24 +103,72 @@ class DeploymentTask(tasks.WrappedCallableTask):
         finally:
             sudo("rm %s" % uploaded[0])
 
-    def __call__(self, *args, **kwargs):
-        return self.run(*args, **kwargs)
+blueprint = DeploymentTask.as_decorator()
 
 
-def blueprint(*args, **kwargs):
-    """
-    Decorator declaring the wrapped function to be a resource generator that should be deployed.
+class DockerBuildTask(FuselageMixin, tasks.WrappedCallableTask):
 
-    May be invoked as a simple, argument-less decorator (i.e. ``@blueprint``) or
-    with arguments customizing its behavior (e.g. ``@blueprint(alias='myalias')``).
-    """
-    invoked = bool(not args or kwargs)
-    if not invoked:
-        func, args = args[0], ()
+    arguments = ['from_image', 'tag']
 
-    task_class = kwargs.pop("task_class", DeploymentTask)
+    def apply_bundle(self, bundle, from_image='ubuntu', tag=None, *args, **kwargs):
+        import docker
+        import tarfile
+        import json
 
-    def wrapper(func):
-        return task_class(func, *args, **kwargs)
+        tar_buffer = six.BytesIO()
+        tar = tarfile.open(mode='w:gz', fileobj=tar_buffer)
 
-    return wrapper if invoked else wrapper(func)
+        def add(name, buf, mode=0o644):
+            ti = tarfile.TarInfo(name=name)
+            ti.size = len(buf.buf)
+            ti.mode = mode
+            tar.addfile(tarinfo=ti, fileobj=buf)
+
+        add("payload.pex", bundle, mode=0o755)
+
+        add("Dockerfile", six.BytesIO("\n".join([
+            "FROM %s" % from_image,
+            "ADD payload.pex /payload.pex",
+            'RUN apt-get update && apt-get install python -y',
+            'RUN /payload.pex',
+            'RUN rm /payload.pex',
+        ])))
+
+        tar.close()
+        tar_buffer.seek(0)
+
+        c = docker.Client(
+            base_url='unix://var/run/docker.sock',
+            version='1.12',
+            timeout=10,
+        )
+
+        build_output = c.build(
+            fileobj = tar_buffer,
+            custom_context = True,
+            stream=True,
+            rm=True,
+            tag=tag,
+        )
+
+        status = None
+        for data in build_output:
+            data = json.loads(data)
+            if 'status' in data:
+                # {u'status': u'Downloading', u'progressDetail': {u'current': 3239, u'start': 141059980, u'total': 133813}, u'id': u'2124c4204a05', u'progress': u'[=>] 32.32 kB/1.338 MB 29s'}
+                if status != data['status']:
+                    status = data['status']
+                    utils.puts(status, flush=True, end='')
+                else:
+                    utils.puts('.', show_prefix=False, flush=True, end='')
+            else:
+                utils.puts('', show_prefix=False, flush=True)
+                status = None
+
+                if 'stream' in data:
+                    utils.puts(data['stream'])
+                elif 'errorDetail' in data:
+                    utils.error(data['error'])
+                    return
+
+docker_container = DockerBuildTask.as_decorator()
